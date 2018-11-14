67ef638c53ec40a19791ac5c02aae2ca8f6e5e60
Aatish Balla <aatishballa@gmail.com>
2018-11-14 16:26:00 -0600

    cus-das setup

--- Ride-share/customer/cus-dashboard.php
+++ Ride-share/customer/cus-dashboard.php
@@ -3,12 +3,49 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    <title>Customer Dashboard</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+        crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
     <script src="main.js"></script>
+    <script>
+
+    </script>
 </head>
 <body>
-    customer dashboard
+
+
+    <div class="container">
+    <div class="row">
+        <div class="col-4">
+            <div class="list-group" id="list-tab" role="tablist">
+            <a class="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="Profile">Profile</a>
+            <a class="list-group-item list-group-item-action" id="list-request-list" data-toggle="list" href="#list-request" role="tab" aria-controls="request">Request a ride</a>
+            <a class="list-group-item list-group-item-action" id="list-scheduled-rides-list" data-toggle="list" href="#list-scheduled-rides" role="tab" aria-controls="scheduled-rides">View scheduled ride</a>
+            <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Settings</a>
+            </div>
+        </div>
+        <div class="col-8">
+            <div class="tab-content" id="nav-tabContent">
+            <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">Personal info</div>
+            <div class="tab-pane fade" id="list-request" role="tabpanel" aria-labelledby="list-request-list">request form</div>
+            <div class="tab-pane fade" id="list-scheduled-rides" role="tabpanel" aria-labelledby="list-scheduled-rides-list">schdeuled ride list, cancel ride button</div>
+            <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">change account info</div>
+            </div>
+        </div>
+</div>
+
+    </div>
+
+     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous">
+    </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous">
+    </script>
 </body>
 </html>
--- Ride-share/user-login.php
+++ Ride-share/user-login.php
@@ -15,31 +15,6 @@
 
 <body>
 
-    <!-- NAV BAR-->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Ride Share</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"
-            aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">About Us</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Service</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="user-login.php">Login</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-
     <div class="row">
         <div class="col">
             <div class="text-center">
